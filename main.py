82559f8632e44417e359955bd2a8d6000c47c50a
Tushar Mazhar <tusharm@inneed.cloud>
2021-02-19 10:16:30 +0600

    path parameter and query parameter implemented in different api endpint

--- main.py
+++ main.py
@@ -6,9 +6,30 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # path operation decorator
-@app.get("/")
+@app.get('/')
 # path operation function
-def read_root():
-    return {"Hello": "World! My first API"}
+def home(limit:int,isTrue:bool):
+    if isTrue:
+        return {"Message":f"Welcome to our site-{limit}"} # Query parameter limit,isTrue
+    else:
+        return {"msg":"Not valid"}
+
+# path operation decorator
+@app.get("/about/{id}")  # path parameter id
+# path operation function
+def read_root(id:int):
+    return {"Hello": id}
+
+# path operation decorator
+@app.get("/blog/{id}/comments") # path parameter id
+# path operation function
+def read_root(id:int):
+    return {"Hello": id}
+
+# path operation decorator
+@app.get("/blog/{id}/comments") # path parameter id
+# path operation function
+def read_root(id:int):
+    return {"Hello": id}
 
 
